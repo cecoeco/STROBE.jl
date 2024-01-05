34ebef1e4e623fcd2f488fcfe1fabc955de060e7
webofceco <mapleselijah9@gmail.com>
2024-01-04 22:31:05 -0500

    app

--- dash/Julia/app.jl
+++ dash/Julia/app.jl
@@ -59,21 +59,13 @@ page = html_div(id = "page-content")
 app.layout = html_div([dcc_location(id = "url"), navbar, fab, page, footer])
 
 callback!(app, Output("page-content", "children"), Input("url", "pathname")) do pathname
-    if pathname == "/home"
-        return home
-    elseif pathname == "/combined"
-        return combined
-    elseif pathname == "/cohort_studies"
-        return cohort_studies
-    elseif pathname == "/case-control_studies"
-        return case_control_studies
-    elseif pathname == "/cross-sectional_studies"
-        return cross_sectional_studies
-    elseif pathname == "/conference_abstracts"
-        return conference_abstracts
-    else
-        return not_found_404
-    end
+    if pathname == "/home" return home
+    elseif pathname == "/combined" return combined
+    elseif pathname == "/cohort_studies" return cohort_studies
+    elseif pathname == "/case-control_studies" return case_control_studies
+    elseif pathname == "/cross-sectional_studies" return cross_sectional_studies
+    elseif pathname == "/conference_abstracts" return conference_abstracts
+    else return not_found_404 end
 end
 
 run_server(app, "0.0.0.0", debug = true)
